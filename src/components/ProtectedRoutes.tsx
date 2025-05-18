c702ac73e322d2f92e183235ac97c24ab935cd2f
Massamba5055 <sabelomaphalala131@gmail.com>
2025-05-18 21:00:05 +0200

    Protected Routes

---
+++ src/components/ProtectedRoutes.tsx
@@ -0,0 +1,30 @@
+import { Navigate, Outlet } from "react-router-dom";
+import type { Roles } from "../types";
+import { useAuth } from "../hooks/useAuth";
+
+interface ProtectedRoutesProps {
+  requiredRole?: Roles | Roles[];
+}
+
+export default function ProtectedRoutes({
+  requiredRole,
+}: ProtectedRoutesProps) {
+  const { user, token, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!token || !user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (requiredRole) {
+    const roles = Array.isArray(requiredRole) ? requiredRole : [requiredRole];
+    if (!roles.includes(user.role)) {
+      return <Navigate to="/unauthorized" replace />;
+    }
+  }
+
+  return <Outlet />;
+}
